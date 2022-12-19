dd4f2e839eef0e9308f42e58f8a58ff7425577d8
BINGOBOUNCE897 <bngbounce@gmail.com>
2022-12-19 10:28:49 +0300

    fix bug 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp;
 function App() {
 
 	useEffect(() => {
 		tg.ready();
 	}, [])
 
-	const onClose = () => {
-		tg.close()
-	}
-
   return (
     <div className="App">
 		ПРИВЕТ МИР
-		<button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
