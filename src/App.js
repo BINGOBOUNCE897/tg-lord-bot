56ceed8d8ea2f3a16ee2b5e926b72f104124c771
BINGOBOUNCE897 <bngbounce@gmail.com>
2022-12-19 11:02:12 +0300

    fix 6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect } from 'react';
 import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className="App">
+		<Header />
 		<button onClick={onToggleButton}>toggle</button>
     </div>
   );
