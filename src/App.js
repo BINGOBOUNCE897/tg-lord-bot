9dcf3b0bcdf580e1b1cf9d91dcae93ebf24dc566
BINGOBOUNCE897 <bngbounce@gmail.com>
2022-12-19 10:07:52 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import './App.css';
+const tg = window.Telegram.WebApp;
+function App() {
+
+	useEffect(() => {
+		tg.ready();
+	}, [])
+
+	const onClose = () => {
+		tg.close()
+	}
+
+  return (
+    <div className="App">
+		ПРИВЕТ МИР
+		<button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
